d12761d7514434b0a989c64015c3bf05c70252d6
Camila Storck <mcamilastorck@gmail.com>
2022-04-09 17:34:32 -0300

    Initial Commit

---
+++ Pippo/PippoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PippoApp.swift
+//  Pippo
+//
+//  Created by Apple  on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct PippoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
